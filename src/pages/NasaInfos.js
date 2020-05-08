197015a977d27cc5b10452d86a6599e998baf622
cmaiw <ch.maiwald80@gmail.com>
2020-05-08 15:19:23 +0200

    remove log to find error

--- src/pages/NasaInfos.js
+++ src/pages/NasaInfos.js
@@ -149,7 +149,6 @@ export default function NasaInfos() {
     const response = await fetch(
       `https://api.nasa.gov/planetary/apod?api_key=${REACT_APP_API_KEY}&date=${query}`
     );
-    console.log(REACT_APP_API_KEY);
     const data = await response.json();
     return data;
   }
