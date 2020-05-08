fc60fcafe05b353979236fc1e431cf8fae7e3e48
cmaiw <ch.maiwald80@gmail.com>
2020-05-08 14:10:20 +0200

    change const in fetch function

--- src/pages/NasaInfos.js
+++ src/pages/NasaInfos.js
@@ -145,9 +145,9 @@ export default function NasaInfos() {
   const [query, setQuery] = React.useState("");
 
   async function getNasaDatabyDate() {
-    const api_key = process.env.REACT_APP_API_KEY;
+    const REACT_APP_API_KEY = process.env.REACT_APP_API_KEY;
     const response = await fetch(
-      `https://api.nasa.gov/planetary/apod?api_key=${api_key}&date=${query}`
+      `https://api.nasa.gov/planetary/apod?api_key=${REACT_APP_API_KEY}&date=${query}`
     );
     const data = await response.json();
     return data;
